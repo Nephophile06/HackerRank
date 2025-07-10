03fe6d9bc8bdcda7c5ce3b03ec8f6ecd5309f93c
Anika Binta Azad Shifa <anikabintaazad24@gmail.com>
2025-07-10 11:36:36 +0600

    Create Basic Data Types.cpp

---
+++ PREPARE BY TOPICS C++/Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    printf("%d\n%lld\n%c\n%.3f\n%.9lf\n", a, b, c, d, e);
+    return 0;
+}
